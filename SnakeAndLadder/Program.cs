fc27f64769203d635009ae5136c5c675b7767802
unknown <asthaagarwal203@gmail.com>
2022-02-14 11:55:29 +0530

    [Astha] Add: SnakeAndLadderUC7

--- SnakeAndLadder/Program.cs
+++ SnakeAndLadder/Program.cs
@@ -8,35 +8,64 @@ namespace SnakeAndLadder
 {
     internal class Program
     {
-        static void Main(string[] args)
+         public static int NewPosition(int options,int DiceNo,int position)
         {
-            Random random = new Random();
-            int position = 0;
-            int diceRolledCount = 0;
             const int LADDER = 1;
             const int NOPLAY = 2;
             const int SNAKE = 3;
-            while (position < 100)
+            switch (options)
+            {
+                case LADDER:
+                    position += DiceNo;
+                    position = Math.Min(position, 100);
+                    break;
+                case NOPLAY:
+                    break;
+                case SNAKE:
+                    position -= DiceNo;
+                    position = Math.Max(position, 0);
+                    break;
+            }
+            return position;
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int FirstPlayerposition = 0,SecondPlayerPosition=0,turn=0;
+            int diceRolledCount = 0;
+           
+            while (FirstPlayerposition < 100&&SecondPlayerPosition<100)
             {
                 int DiceNo = random.Next(1, 7);
                 int options = random.Next(1, 4);
-                switch (options)
+                if (turn == 0)
                 {
-                    case LADDER:
-                        position += DiceNo;
-                        position = Math.Min(position,100);
-                        break;
-                    case NOPLAY:
-                        break;
-                    case SNAKE:
-                        position -= DiceNo;
-                        position = Math.Max(position,0);
-                        break;
+                    FirstPlayerposition = NewPosition(options, DiceNo, FirstPlayerposition);
+                    turn = 1;
+                    if (options == 1)
+                    {
+                        DiceNo = random.Next(1, 7);
+                        FirstPlayerposition = NewPosition(options, DiceNo, FirstPlayerposition);
+                    }
+                }
+                else
+                {
+                    SecondPlayerPosition = NewPosition(options, DiceNo, SecondPlayerPosition);
+                    turn = 0;
+                         if (options == 1)
+                    {
+                        DiceNo = random.Next(1, 7);
+                        SecondPlayerPosition = NewPosition(options, DiceNo, SecondPlayerPosition);
+                    }
+
                 }
                 diceRolledCount++;
-                Console.WriteLine("The current position is "+position);
+                Console.WriteLine("Dice No is "+diceRolledCount+" position of player one is "+FirstPlayerposition+" position of second player is "+SecondPlayerPosition);
             }
-            Console.WriteLine("No of times dice was rolled " + diceRolledCount);
+            if (FirstPlayerposition == 100)
+                Console.WriteLine("Player One won");
+                else
+                Console.WriteLine("Player Two won ");
         }
     }
 }
